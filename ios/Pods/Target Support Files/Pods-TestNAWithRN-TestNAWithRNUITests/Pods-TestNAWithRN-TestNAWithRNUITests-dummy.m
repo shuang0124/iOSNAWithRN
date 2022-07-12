ad19dbd8b20c76ce89cb32623cbce366cb0ca470
lushuang <lushuang@igetcool.com>
2022-07-12 15:46:38 +0800

    delete pod

--- ios/Pods/Target Support Files/Pods-TestNAWithRN-TestNAWithRNUITests/Pods-TestNAWithRN-TestNAWithRNUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestNAWithRN_TestNAWithRNUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TestNAWithRN_TestNAWithRNUITests
-@end
--- ios/Pods/Target Support Files/Pods-TestNAWithRNTests/Pods-TestNAWithRNTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestNAWithRNTests : NSObject
-@end
-@implementation PodsDummy_Pods_TestNAWithRNTests
-@end
